4dfa8629a38b2db94c43fa59fe32c8624c2f342a
Greg Milligan <xybersolve@gmail.com>
2015-01-18 21:39:35 -0500

    add reminder module, navbar menu update for new features

---
+++ client/development/app/reminder/reminder-routes.js
@@ -0,0 +1,17 @@
+(function(window, angular, undefined) {  'use strict';
+
+  angular
+    .module('app')
+    .config(['$stateProvider', reminderRoute]);
+
+  function reminderRoute($stateProvider){
+    $stateProvider
+      .state('reminders', {
+        url: '/reminders',
+        templateUrl: 'app/reminder/reminder-main.html',
+        controller: 'ReminderMainCtrl as reminderMain'
+      });
+
+  }
+
+})(window, window.angular);
