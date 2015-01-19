5aa3e05ad543568bcc1b8dbb5b77936790c10b81
Greg Milligan <xybersolve@gmail.com>
2015-01-18 21:25:04 -0500

    add report feature - stubbed out

---
+++ client/development/app/report/menu/report-menu-controller.js
@@ -0,0 +1,11 @@
+(function(window, angular, undefined){ 'use strict';
+
+  angular
+    .module('app')
+    .controller('ReportMenuCtrl', [function(){
+      var vm = this;
+
+
+   }]);
+
+})(window, window.angular);
---
+++ client/development/app/report/report-routes.js
@@ -0,0 +1,21 @@
+(function(window, angular, undefined) {  'use strict';
+
+  angular
+    .module('app')
+    .config(['$stateProvider', '$urlRouterProvider', reportRoutes]);
+
+  function reportRoutes($stateProvider, $urlRouterProvider){
+
+    $stateProvider
+      .state('report', {
+        url: '/report',
+        template: '<div ui-view></div>'
+      })
+      .state('report.menu', {
+        url: '/menu',
+        templateUrl: 'app/report/menu/report-menu.html',
+        controller: 'ReportMenuCtrl as reportMenu'
+      })
+  }
+
+})(window, window.angular);
