3c0bee1e7fbfffd3e814da801b7c560c7c507d2e
Greg Milligan <xybersolve@gmail.com>
2015-01-18 21:26:22 -0500

    add feature: tracking

---
+++ client/development/app/tracking/tracking-routes.js
@@ -0,0 +1,18 @@
+(function(window, angular, undefined) {  'use strict';
+
+  angular
+    .module('app')
+    .config(['$stateProvider', trackingRoutes]);
+
+  function trackingRoutes($stateProvider ){
+
+    $stateProvider
+      .state('tracking', {
+        url: '/tracking',
+        templateUrl: 'app/tracking/tracking-main.html',
+        controller: 'TrackingMainCtrl as trackingMain'
+      })
+
+  }
+
+})(window, window.angular);
