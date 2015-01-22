2cd210f530a7af21ba6895e943368ceb395ee8fd
Greg Milligan <xybersolve@gmail.com>
2015-01-22 18:56:00 -0500

    make lead update multi purpose with shared factory

--- client/development/app/lead/edit/lead-edit-controller.js
+++ client/development/app/lead/edit/lead-edit-controller.js
@@ -17,19 +17,32 @@
         vm.lead = lead;
         vm.ranks = attributeSvc.ranks;
 
-        // save or update leads
-        // shared by sub-document updates
-        function updateLead(item , cb){
-          var copy = angular.copy(vm.lead);
-          copy.update().then(function(result) {
-            if(result.success===true){
-              notifySvc.success('Updated "' + item + '".');
-              if(cb) cb();
-            } else {
-              notifySvc.error('Oops! Unable to update "' + item + '"!');
-            }
-          });
+        // save or update entities - leads & sub-documents (tracking & reminders)
+        // main update factory
+        function updateItem(itemName){
+          var item = itemName;
+          return function(cb){
+            var copy = angular.copy(vm.lead);
+            copy.update().then(function(result) {
+              if(result.success===true){
+                notifySvc.success('Updated "' + item + '".');
+                if(cb) cb();
+              } else {
+                notifySvc.error('Oops! Unable to update "' + item + '"!');
+              }
+            });
+          };
         }
+        // specific update routines
+        var updateLead = updateItem('lead');
+        var updateTracking = updateItem('tracking');
+        var updateReminder = updateItem('reminder');
+
+        function gotUpdate(){
+          // UI update behavior - back to list?
+          if(autoClose) $state.go('leads.list');
+        }
+
         // event: update main lead
         vm.update = function() {
           updateLead('lead', gotUpdate);
@@ -41,11 +54,12 @@
         vm.trackingTypes = attributeSvc.trackingTypes;
         vm.track = new trackingSvc();
 
-        vm.addTracking = function() {
+        vm.addTracking = function(isValid) {
+          if(!isValid) return;
           // keep business logic in service
           vm.lead.addTracking(vm.track);
           // update the lead (parent document)
-          updateLead('tracking', function() {
+          updateTracking(function() {
             vm.track = new trackingSvc();
             gotUpdate();
           });
@@ -53,7 +67,7 @@
 
         vm.deleteTracking = function(track) {
           vm.lead.deleteTracking(track);
-          updateLead('tracking', gotUpdate);
+          updateTracking(gotUpdate);
         };
 
         /* ---------------------------------------------------------
@@ -62,9 +76,10 @@
         vm.reminderTypes = attributeSvc.reminderTypes;
         vm.reminder = new reminderSvc();
 
-        vm.addReminder = function () {
+        vm.addReminder = function(isValid) {
+          if(!isValid) return;
           vm.lead.addReminder(vm.reminder);
-          updateLead('reminder', function() {
+          updateReminder(function() {
             vm.reminder = new reminderSvc();
             gotUpdate();
           });
@@ -72,17 +87,12 @@
 
         vm.deleteReminder = function(reminder) {
           vm.lead.deleteReminder(reminder);
-          updateLead('reminder', gotUpdate);
+          updateReminder(gotUpdate);
         };
 
         vm.toggleReminderClosed = function(reminder) {
-          updateLead('reminder', gotUpdate)
+          updateReminder(gotUpdate);
         };
-
-        function gotUpdate(){
-          // UI update behavior - back to list?
-          if(autoClose) $state.go('leads.list');
-        }
 
       }]);
 })(window, window.angular);
